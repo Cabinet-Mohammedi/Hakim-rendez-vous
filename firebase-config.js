0450cf3f2c6c38424a047920d81ec914041d04a4
Cabinet-Mohammedi <cabinet.mohammedi17@gmail.com>
2025-12-14 15:54:33 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,11 @@
-<script>
-  // Configuration Firebase
-  const firebaseConfig = {
-    apiKey: "AIzaSyDaituJimoLNpkMYvr1u4KJC8XEJgbrGZA",
-    authDomain: "clinique-9f351.firebaseapp.com",
-    databaseURL: "https://clinique-9f351-default-rtdb.firebaseio.com",
-    projectId: "clinique-9f351",
-    storageBucket: "clinique-9f351.appspot.com",
-    messagingSenderId: "537502134144",
-    appId: "1:537502134144:web:a7d0ba7fb48f97b2775b65"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-</script>
+// firebase-config.js
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDaituJimoLNpkMYvr1u4KJC8XEJgbrGZA",
+  authDomain: "clinique-9f351.firebaseapp.com",
+  databaseURL: "https://clinique-9f351-default-rtdb.firebaseio.com",
+  projectId: "clinique-9f351",
+  storageBucket: "clinique-9f351.appspot.com",
+  messagingSenderId: "537502134144",
+  appId: "1:537502134144:web:a7d0ba7fb48f97b2775b65"
+};
